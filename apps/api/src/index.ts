833817154d6cc9731a483ce6435920512e5e362a
DiscoverGroup <romanolantano.discovergrp@gmail.com>
2026-02-20 11:55:00 +0800

    fix: add NextFunction to express import

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
